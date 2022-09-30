b29004d9f5b4eaa67bb8c2c711c976f490498be3
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-30 18:34:01 +0530

    Add files via upload
    "Write a C# program that implements an IVehicle interface with two methods, one for Drive of type void and another for Refuel of type bool. 
    The Refuel method takes an input parameter of type integer with the amount of petrol/diesel to refuel. 
    Then create a Car class with a builder that receives a parameter with the car's starting fuel(petrol/diesel) amount and implements
     the Drive and Refuel methods of the car.
    
    The Drive method will print on the screen that the car is Driving, if the fuel(petrol/diesel) is greater than 0. 
    The Refuel method will increase the fuel(petrol/diesel) of the car and return true.
    
    To carry out the tests, create an object of type Car with 0 of fuel(petrol/diesel) in the Main program 
    and ask the user for an amount of fuel(petrol/diesel) to refuel, finally execute the Drive method of the car."

---
+++ Refuel30/Refuel30/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Refuel30
+{
+    public class car
+    {
+        public interface IVehicle
+        {
+            void Drive();
+            bool Refuel(int amt);
+        }
+        public class car1 : IVehicle
+        {
+            public int Fuel { get; set; }
+
+            public car1(int fuel)            {
+                Fuel = fuel;
+            }
+
+            public void Drive()
+            {
+                if (Fuel > 0)
+                {
+                    Console.WriteLine("start driving");
+                }
+                else
+                {
+                    Console.WriteLine("No fuel");
+                }
+            }
+
+            public bool Refuel(int amt)
+            {
+                Fuel = Fuel + amt;
+                return true;
+            }
+
+
+            static void Main(string[] args)
+            {
+                car1 c1 = new car1(0);
+                int fuel = int.Parse(Console.ReadLine());
+                if (c1.Refuel(fuel))
+                {
+                    c1.Drive();
+                }
+            }
+        }
+    }
+}
