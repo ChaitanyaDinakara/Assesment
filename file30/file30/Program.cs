8274e2dd35a8544c6d9c274111d7ab6118172470
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-30 18:35:30 +0530

    Add files via upload
    "Write a C# program to create a file with a name given by the user. Add atleast 10 lines of content to the file. 
    Get 2 inputs from the user, old word and new word and replace the old word with the new word throughout the file. 
    You can show a little bit of content to the user as hints for the old word. "

---
+++ file30/file30/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace file30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the name");
+            string name= Console.ReadLine();
+            StreamWriter sw = new StreamWriter("C:\\Users\\chaitanya.dinakara\\OneDrive - Entain Group\\Documents\\ name.txt");
+            sw.WriteLine("This is Chaitanya");
+            sw.WriteLine("My age is 22");
+            sw.WriteLine("I love dancing");
+            sw.WriteLine("I love glass painting");
+            sw.WriteLine("I am hyped about learning new things");
+            sw.WriteLine("make everyone happy");
+            sw.WriteLine("enjoy your day");
+            sw.WriteLine("Have fun");
+            sw.WriteLine("Knowledge is power");
+            sw.WriteLine("have a great day");
+        }
+    }
+}
