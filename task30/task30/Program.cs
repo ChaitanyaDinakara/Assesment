b1d3df8eb1e3de8381bd9730f9e5aacec56d05ef
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-30 18:32:34 +0530

    Add files via upload
    "Create a C# program to:
    The main task is to get a stage / event ready with the following tasks:
    Flower decorations to be completed on stage.
    Chief guest to be picked up from the airport. 
    Co-ordinate with the caterers and set up the food area. 
    Arrange the gifts to be distributed in the stage after the decorations are over. 
    Start the function only when the Chief guest has arrived at the venue. "

---
+++ task30/task30/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading;
+
+namespace task30
+{
+    internal class Program
+    {
+        public void flowerdecor()
+        {
+            Console.WriteLine("complete the decoration");
+            Thread.Sleep(3000);
+        }
+        public void chiefguest()
+        {
+            Console.WriteLine("picked the guest from airport");
+            Thread.Sleep(2000);
+            
+        }
+
+        public void coordinate()
+        {
+            Console.WriteLine("set up the food");
+        }
+
+        public void gifts()
+        {
+            Console.WriteLine("Arrange the gifts");
+            Thread.Sleep(3000);
+        }
+        static async Task start()
+        {
+             Console.WriteLine("start the function");
+            
+        }
+
+        static async Task Main(string[] args)
+        {
+            Program p =new Program();
+            
+            p.flowerdecor();
+            p.chiefguest();
+            p.coordinate();
+            p.gifts();
+            await start();
+        }
+    }
+}
