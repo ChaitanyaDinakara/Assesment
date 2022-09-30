f44615b83d4da65f3d768e8332d1b92e202eb372
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-30 18:31:10 +0530

    Add files via upload
    "Write a C# program that uses a Queue to store a list of people. Create a class called Person.cs with two properties (name and age) 
    and a ToString() method to print the result.
    
    Then define a Queue and request the information (name and age) of 5 people from the user, you will have to 
    store the information in the queue.  Finally print the result of the queue on screen. "

---
+++ Oueue30/Oueue30/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Oueue30
+{
+    internal class Person
+    {
+
+        public static void Main(string[] args)
+        {
+            Queue q = new Queue();
+            int total = 5;
+
+            for (int i = 0; i < total; i++)
+            {
+
+
+                string name = Console.ReadLine();
+                Console.WriteLine(name);
+                int age = int.Parse(Console.ReadLine());
+                Console.WriteLine(age);
+
+                q.Enqueue(new Person() { Name = name, Age = age });
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                Person p = (Person)q.Dequeue();
+                Console.WriteLine(p.ToString());
+            }
+        }
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+            public override string ToString()
+            {
+                return Name + "  " + Age;
+            }
+        }
+    }
+}
+
+
