8c5ca774cae1d3434a9e9cc181637a5e17523424
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-30 18:38:56 +0530

    Add files via upload
    "Create a C# program to:
    Create an Employee class and add about 5 employees. Write a method to give promotion. 
    Get a value from the user as a trigger to give promotion to selected employees. Use Delegates
    "

---
+++ employee30/employee30/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace employee30
+{
+    internal class Employee
+    {
+        public delegate void cal(string a,int b);
+
+        public void entry()
+        {
+
+            Dictionary<string, string> emp = new Dictionary<string, string>();
+            {
+                emp.Add("Kevin", "Good");
+                emp.Add("David", "Good");
+                emp.Add("Harish", "Good");
+                emp.Add("Manoj", "bad");
+                emp.Add("Priya", "bad");
+
+                foreach (KeyValuePair<string,string> keyValuePair in emp) ;
+                Console.WriteLine(KeyValuePair.key.ToString + " " + KeyValuePair.Value);
+            }
+
+        }
+        public void promotion( string a)
+        {
+            string keyValue = "Good";
+            if(keyValue == "Good")
+            Console.WriteLine("promoted");
+
+        }
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.promotion("good");
+        }
+    }
+}
