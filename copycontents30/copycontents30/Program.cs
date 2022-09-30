7fefea3a469cbf1f0405c503beb6c786db79ed3f
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-30 18:36:40 +0530

    Add files via upload
    Write a C# program to copy the contents of one file to another file and encrypt the new file. Have any rule to differentiate the contents along with the encryption.

---
+++ copycontents30/copycontents30/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace copycontents30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("C:\\Users\\chaitanya.dinakara\\OneDrive - Entain Group\\Documents\\file.txt",FileMode.OpenOrCreate);
+            fs.Close();
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine("Today is Friday\n", "have a nice day");
+            sw.Close();
+            FileStream fs1 = new FileStream("C:\\Users\\chaitanya.dinakara\\OneDrive - Entain Group\\Documents\\file1.txt", FileMode.OpenOrCreate);
+            StreamWriter sw2 = new StreamWriter(fs1);
+            sw2.WriteLine("Today is Thursday\n", "Enjoy ur day");
+            sw2.Close();
+            fs1.CopyTo(fs);
+            fs.Close();
+            fs1.Close();
+
+        }
+    }
+}
