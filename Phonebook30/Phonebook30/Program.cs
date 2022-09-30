9184901503bdeb3784f0f20a20be604c07d21d39
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-30 18:41:42 +0530

    Add files via upload
    "Write a C# program to create a Phonebook. 
    The Phonebook should take inputs from the user and store their numbers. 
    Also ask for the user age and store their numbers as Students in a sub class. If the user gives a profession, store the numbers in the Service subclass. 
    Display the numbers as a collection.

---
+++ Phonebook30/Phonebook30/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace Phonebook30
+{
+    internal class Phonebook
+    {
+        class Student : Phonebook
+        {
+
+            void display()
+            {
+                List<string> ItemList = new List<string>();
+                List<int> Items = new List<int>();
+                List<string> item = new List<string>();
+                Console.WriteLine("enter the items to be added");
+                for (int i = 0; i < 10; i++)
+                {
+                    ItemList.Add(i.ToString());
+                   
+                }
+
+                for (int j = 0; j < 10; j++)
+                {
+                    Items.Add(j);
+                }
+                for (int k = 0; k < 10; k++)
+                {
+                    item.Add(k.ToString());
+                }
+            }
+
+                static void Main(string[] args)
+                {
+                 Student s = new Student();
+                    s.display();
+                }
+            }
+        }
+    }
+
+
+            
+        
+    
