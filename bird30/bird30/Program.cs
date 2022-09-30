32f29051fa1ee9d38e44ffe1b32c7fd2e2962f39
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-30 18:29:30 +0530

    Add files via upload
    "Write a C# program that implements an abstract class Bird that has a Name property of type text and three methods SetName (string name), GetName and Fly. 
    The Eat method will be an abstract method of type void.
    You will also need to create a Crow/Parrot class that implements the above Bird class and the Fly method should say that the crow/parrot is flying. 
    To test the program ask the user for a bird name and create a new Crow/Parrot type object from the Main program, 
    give the Crow/Parrot object a name, and then execute the GetName and Fly methods."

---
+++ bird30/bird30/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Xml.Linq;
+using static bird30.abstractclass;
+
+namespace bird30
+{
+    public class abstractclass
+    {
+        
+        public class Crow : Bird
+        {
+            public override void Eat()
+            {
+                Console.WriteLine("eating");
+                Console.WriteLine("the bird is about to fly");
+            }
+
+            
+        }
+
+        public abstract class Bird
+        {
+            private string Name;
+
+            public void SetName(string name)
+            {
+                Name = name;
+            }
+
+            public string GetName()
+            {
+                return Name;
+            }
+           
+            public abstract void Eat();
+        }
+
+           public static void Main(string[] args)
+            {
+                Crow c = new Crow();
+            Console.WriteLine("enter the name of the crow");
+               c.SetName(Console.ReadLine());
+                Console.WriteLine(c.GetName());
+                c.Eat();
+            }
+
+        }
+    }
+
+    
