a8978f24f92472e91bd6ae97ee2f9b1925f3f496
ChaitanyaDinakara <113330895+ChaitanyaDinakara@users.noreply.github.com>
2022-09-30 18:27:34 +0530

    Add files via upload
    Write a C# program to find the maximum and minimum value in an array

---
+++ array30/array30/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace array30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            int i, max, min, n;
+            Console.WriteLine("enter the number of elements to be stored");
+            n= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the number of elements is:", n);
+
+            for (i = 0; i < arr.Length-1; i++)
+            {
+                Console.WriteLine("elements", i);
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            max = arr[0];
+            min = arr[0];
+
+            for (i = 0; i < arr.Length-1; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+            if (arr[i] < min)
+            {
+                min = arr[i];
+            }
+
+            Console.WriteLine("max element in the array is:{0}\n", max);
+            Console.WriteLine("minimum element in the array is:{0}\n",min);
+        }
+    }
+}
